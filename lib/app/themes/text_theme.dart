49a4c6e0a4511df4fa26c91f18fd6734ad73f45e
Ngurah Deva <wirandanaputra@gmail.com>
2022-06-13 20:51:21 +0800

    add text theme

---
+++ lib/app/themes/text_theme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var projectTextTheme = TextTheme(
+  headline1: GoogleFonts.nunito(
+    fontSize: 96,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5
+  ),
+  headline2: GoogleFonts.nunito(
+    fontSize: 60,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5
+  ),
+  headline3: GoogleFonts.nunito(
+    fontSize: 48,
+    fontWeight: FontWeight.w400
+  ),
+  headline4: GoogleFonts.nunito(
+    fontSize: 34,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25
+  ),
+  headline5: GoogleFonts.nunito(
+    fontSize: 24,
+    fontWeight: FontWeight.w400
+  ),
+  headline6: GoogleFonts.nunito(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15
+  ),
+  subtitle1: GoogleFonts.nunito(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15
+  ),
+  subtitle2: GoogleFonts.nunito(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1
+  ),
+  bodyText1: GoogleFonts.nunito(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5
+  ),
+  bodyText2: GoogleFonts.nunito(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25
+  ),
+  button: GoogleFonts.nunito(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25
+  ),
+  caption: GoogleFonts.nunito(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4
+  ),
+  overline: GoogleFonts.nunito(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5
+  ),
+);
