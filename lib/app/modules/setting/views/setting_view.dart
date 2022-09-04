d011856f74889302dee2a5fe0d923f2cb4539eeb
Gusti Ngurah Deva Wirandana Putra <deva@suitmedia.com>
2022-09-05 01:43:32 +0800

    add setting widget

--- lib/app/modules/setting/views/setting_view.dart
+++ lib/app/modules/setting/views/setting_view.dart
@@ -1,3 +1,4 @@
+import 'package:buahtangan/app/modules/setting/widgets/setting_widget.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -22,135 +23,97 @@ class SettingView extends GetView<SettingController> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                stream: controller.streamProfile(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircleAvatar(
-                        radius: 80,
-                        backgroundColor: slate500,
-                      ),
-                    );
-                  }
+              // StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              //   stream: controller.streamProfile(),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.connectionState == ConnectionState.waiting) {
+              //       return Center(
+              //         child: CircleAvatar(
+              //           radius: 80,
+              //           backgroundColor: slate500,
+              //         ),
+              //       );
+              //     }
 
-                  Map<String, dynamic>? data = snapshot.data!.data();
+              //     Map<String, dynamic>? data = snapshot.data!.data();
 
-                  return Column(
-                    children: [
-                      Container(
-                        decoration: shadowDecoration(),
-                        margin: const EdgeInsets.all(25.0),
-                        child: Center(
-                          child: Stack(
-                            children: [
-                              CircleAvatar(
-                                radius: 80,
-                                backgroundImage: NetworkImage(
-                                  data?["profile"] != null 
-                                  ? data!["profile"] 
-                                  : "https://ui-avatars.com/api/?size=120&name=${data!["name"]}"
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                right: 10,
-                                child: CircleAvatar(
-                                  backgroundColor: secondaryColor,
-                                  radius: 20,
-                                  child: IconButton(
-                                    onPressed: () => controller.pickImage(), 
-                                    icon: Icon(
-                                      Icons.edit,
-                                      color: onSecondaryColor,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Text(
-                        data["name"], 
-                        style: projectTextTheme.headline6?.copyWith(
-                          color: onPrimaryColor
-                        ),
-                      ),
-                    ],
-                  );
-                }
-              ),
+              //     return Column(
+              //       children: [
+              //         Container(
+              //           decoration: shadowDecoration(),
+              //           margin: const EdgeInsets.all(25.0),
+              //           child: Center(
+              //             child: Stack(
+              //               children: [
+              //                 CircleAvatar(
+              //                   radius: 80,
+              //                   backgroundImage: NetworkImage(
+              //                     data?["profile"] != null 
+              //                     ? data!["profile"] 
+              //                     : "https://ui-avatars.com/api/?size=120&name=${data!["name"]}"
+              //                   ),
+              //                 ),
+              //                 Positioned(
+              //                   bottom: 0,
+              //                   right: 10,
+              //                   child: CircleAvatar(
+              //                     backgroundColor: secondaryColor,
+              //                     radius: 20,
+              //                     child: IconButton(
+              //                       onPressed: () => controller.pickImage(), 
+              //                       icon: Icon(
+              //                         Icons.edit,
+              //                         color: onSecondaryColor,
+              //                       ),
+              //                     ),
+              //                   ),
+              //                 ),
+              //               ],
+              //             ),
+              //           ),
+              //         ),
+              //         Text(
+              //           data["name"], 
+              //           style: projectTextTheme.headline6?.copyWith(
+              //             color: onPrimaryColor
+              //           ),
+              //         ),
+              //       ],
+              //     );
+              //   }
+              // ),
               const SizedBox(height: 40),
               Container(
                 width: Get.width,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                  boxShadow: [dropShadow()],
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30), 
-                    topRight: Radius.circular(30), 
-                  )
-                ),
+                padding: const EdgeInsets.only(bottom: 50, top: 24),
+                decoration: whiteContainerDecoration(),
                 child: Column(
                   children: [
-                    const SizedBox(height: 24),
                     SettingWidgets(
-                      leading : const Icon(IconlyBold.edit),
-                      title : Text("Edit Profile", style: projectTextTheme.bodyText1),
-                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                      route: Routes.EDIT_PROFIL,
+                      leading : IconlyBold.edit,
+                      title : "Edit Profile",
+                      onTap: () => Get.toNamed(Routes.EDIT_PROFIL),
                     ),
                     SettingWidgets(
-                      leading : const Icon(IconlyBold.lock),
-                      title : Text("Change Password", style: projectTextTheme.bodyText1),
-                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                      route: Routes.CHANGE_PASSWORD,
-                    ),
-                    Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [dropShadow()],
-                        color: surfaceColor
-                      ),
-                      margin: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-                      child: Center(
-                        child: ListTile(
-                          onTap: () => Get.defaultDialog(title: "Halo"),
-                          leading: const Icon(IconlyBold.message),
-                          title: Text("Send Feedback", style: projectTextTheme.bodyText1),
-                          trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                          iconColor: onSurfaceColor,
-                          textColor: onSurfaceColor,
-                        ),
-                      ),
+                      leading : IconlyBold.lock,
+                      title : "Change Password",
+                      onTap: () => Get.toNamed(Routes.CHANGE_PASSWORD),
                     ),
                     SettingWidgets(
-                      leading : const Icon(IconlyBold.infoCircle),
-                      title : Text("About", style: projectTextTheme.bodyText1),
-                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                      route: Routes.ABOUT,
+                      leading : IconlyBold.lock,
+                      title : "Send Feedback",
+                      onTap: () => Get.defaultDialog(title: "Halo"),
                     ),
-                    Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [dropShadow()],
-                        color: surfaceColor
-                      ),
-                      margin: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-                      child: Center(
-                        child: ListTile(
-                          onTap: () => controller.logout(),
-                          leading: const Icon(IconlyBold.logout),
-                          title: Text("Logout", style: projectTextTheme.bodyText1),
-                          iconColor: onSurfaceColor,
-                          textColor: onSurfaceColor,
-                        ),
-                      ),
+                    SettingWidgets(
+                      leading : IconlyBold.infoCircle,
+                      title : "About",
+                      onTap: () => Get.toNamed(Routes.ABOUT),
                     ),
-                    const SizedBox(height: 50),
+                    SettingWidgets(
+                      leading : IconlyBold.logout,
+                      title : "Logout",
+                      onTap: () => controller.logout(),
+                    ),
                   ],
                 ),
               ),
@@ -162,41 +125,4 @@ class SettingView extends GetView<SettingController> {
   }
 }
 
-class SettingWidgets extends StatelessWidget {
-  final Icon leading;
-  final Text title;
-  final Icon trailing;
-  final String route;
 
-  const SettingWidgets({
-    required this.leading,
-    required this.title,
-    required this.trailing,
-    required this.route,
-    Key? key,
-  }) : super(key: key);
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [dropShadow()],
-        color: surfaceColor
-      ),
-      margin: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-      child: Center(
-        child: ListTile(
-          iconColor: onSurfaceColor,
-          textColor: onSurfaceColor,
-          onTap: ()=> Get.toNamed(route),
-          leading: leading,
-          title: title,
-          trailing: trailing,
-        ),
-      ),
-    );
-  }
-}
