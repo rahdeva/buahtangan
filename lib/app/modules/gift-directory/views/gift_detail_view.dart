23cbb4f4c6974daa97784706988aaf7d5a5ff474
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-18 22:13:04 +0800

    add picture

--- lib/app/modules/gift-directory/views/gift_detail_view.dart
+++ lib/app/modules/gift-directory/views/gift_detail_view.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 import '../../../themes/color_theme.dart';
 import '../../../themes/decoration.dart';
@@ -51,8 +52,7 @@ class GiftDetailView extends GetView<GiftDetailController> {
               ],
             ),
             Container(
-                width: Get.width,
-                height: Get.height,
+                width: 100.w,
                 decoration: BoxDecoration(
                   color: backgroundColor,
                   boxShadow: [dropShadow()],
@@ -61,15 +61,23 @@ class GiftDetailView extends GetView<GiftDetailController> {
                     topRight: Radius.circular(30), 
                   )
                 ),
+                padding: const EdgeInsets.fromLTRB(24, 40, 24, 24),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      margin: const EdgeInsets.fromLTRB(40, 60, 40, 28),
-                      child: Text(
-                        "Gift Detail",
-                        style: projectTextTheme.headline6?.copyWith(
-                          color: onBackgroundColor
-                        ),
+                      height: 200,
+                      width: 100.w,
+                      decoration: BoxDecoration(
+                        boxShadow: [dropShadow()],
+                        borderRadius: BorderRadius.circular(15),
+                        image: const DecorationImage(
+                          image: NetworkImage(
+                            "https://picsum.photos/300/300",
+                          ),
+                          fit: BoxFit.cover
+                        )
                       ),
                     ),
                   ]
