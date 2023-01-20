877d1217b63c30c8263f4b02e7bf606f87129d38
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-20 15:06:21 +0800

    adjust dashboard controller

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -3,26 +3,30 @@ import 'package:get/get.dart';
 import '../controllers/dashboard_controller.dart';
 import '../../../widgets/bottom-navigation/bottom_nav_bar.dart';
 
-class DashboardView extends GetView<DashboardController> {
+class DashboardView extends StatelessWidget {
   const DashboardView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GetBuilder(
-        id: 'nav-bar',
-        init: controller,
-        builder: (_) {
-          return controller.tabView[controller.tabIndex];
-        },
-      ),
-      bottomNavigationBar: GetBuilder(
-        id: 'bottom-nav-bar',
-        init: controller,
-        builder: (_) {
-          return BottomNavBarWidget(controller: controller);
-        },
-      ),
+    return GetBuilder<DashboardController>(
+      builder: (controller) {
+        return Scaffold(
+          body: GetBuilder(
+            id: 'nav-bar',
+            init: controller,
+            builder: (_) {
+              return controller.tabView[controller.tabIndex];
+            },
+          ),
+          bottomNavigationBar: GetBuilder(
+            id: 'bottom-nav-bar',
+            init: controller,
+            builder: (_) {
+              return BottomNavBarWidget(controller: controller);
+            },
+          ),
+        );
+      }
     );
   }
 }
