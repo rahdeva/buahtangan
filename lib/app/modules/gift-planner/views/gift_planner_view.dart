27741c55faabdc9b63bbbd697ce1999f3fe19892
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-20 15:01:17 +0800

    add smartrefresher gift planner

--- lib/app/modules/gift-planner/views/gift_planner_view.dart
+++ lib/app/modules/gift-planner/views/gift_planner_view.dart
@@ -3,6 +3,7 @@ import 'package:buahtangan/app/routes/app_pages.dart';
 import 'package:buahtangan/app/widgets/colored_status_bar.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../../themes/color_theme.dart';
@@ -19,46 +20,50 @@ class GiftPlannerView extends GetView<GiftPlannerController> {
     return ColoredStatusBar(
       child: Scaffold(
         backgroundColor: primaryColor,
-        body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 16),
-            Container(
-              width: 100.w,
-              padding: const EdgeInsets.all(24),
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                boxShadow: [dropShadow()],
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(30), 
-                  topRight: Radius.circular(30), 
-                )
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Gift Planner",
-                      style: projectTextTheme.headline6?.copyWith(
-                      color: onBackgroundColor,
-                      fontWeight: FontWeight.bold
+        body: SmartRefresher(
+          controller: controller.refreshController,
+          onRefresh: controller.refreshPage,
+          child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 16),
+              Container(
+                width: 100.w,
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: backgroundColor,
+                  boxShadow: [dropShadow()],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30), 
+                    topRight: Radius.circular(30), 
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Gift Planner",
+                        style: projectTextTheme.headline6?.copyWith(
+                        color: onBackgroundColor,
+                        fontWeight: FontWeight.bold
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "Be more organized in your gift-giving",
-                    style: projectTextTheme.subtitle2?.copyWith(
-                      color: slate500,
+                    const SizedBox(height: 8),
+                    Text(
+                      "Be more organized in your gift-giving",
+                      style: projectTextTheme.subtitle2?.copyWith(
+                        color: slate500,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  Divider(color: slate400),
-                  ListPlannerBuilder(controller),
-                ]
-              ),
-            )
-          ],
-        ),
+                    const SizedBox(height: 16),
+                    Divider(color: slate400),
+                    ListPlannerBuilder(controller),
+                  ]
+                ),
+              )
+            ],
+          ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (){
--- lib/app/modules/home/views/home_result_view.dart
+++ lib/app/modules/home/views/home_result_view.dart
@@ -1,6 +1,7 @@
 import 'package:buahtangan/app/widgets/colored_status_bar.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../../themes/color_theme.dart';
@@ -18,38 +19,42 @@ class HomeResultView extends GetView<HomeResultController> {
     return ColoredStatusBar(
       child: Scaffold(
         backgroundColor: primaryColor,
-        body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const BackButtonWidget(),
-            Container(
-                width: 100.w,
-                height: 100.h,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                  boxShadow: [dropShadow()],
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30), 
-                    topRight: Radius.circular(30), 
-                  )
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(40, 60, 40, 28),
-                      child: Text(
-                        "Gift Finder Result",
-                        style: projectTextTheme.headline6?.copyWith(
-                          color: onBackgroundColor
+        body: SmartRefresher(
+          controller: controller.refreshController,
+          onRefresh: controller.refreshPage,
+          child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const BackButtonWidget(),
+              Container(
+                  width: 100.w,
+                  height: 100.h,
+                  decoration: BoxDecoration(
+                    color: backgroundColor,
+                    boxShadow: [dropShadow()],
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30), 
+                      topRight: Radius.circular(30), 
+                    )
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(40, 60, 40, 28),
+                        child: Text(
+                          "Gift Finder Result",
+                          style: projectTextTheme.headline6?.copyWith(
+                            color: onBackgroundColor
+                          ),
                         ),
                       ),
-                    ),
-                  ]
-                ),
-            )
-          ],
-        ),
+                    ]
+                  ),
+              )
+            ],
+          ),
+          ),
         ),
       ),
     );
