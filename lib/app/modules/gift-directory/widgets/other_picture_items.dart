acd3c3002ce4946f08f8dc6498b3eb2621e72f98
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-20 08:21:37 +0800

    add placeholder and shimmer on detail gift

--- lib/app/modules/gift-directory/widgets/other_picture_items.dart
+++ lib/app/modules/gift-directory/widgets/other_picture_items.dart
@@ -1,6 +1,8 @@
 import 'package:buahtangan/app/themes/decoration.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:sizer/sizer.dart';
 
 class OtherPictureListItem extends StatelessWidget {
@@ -33,11 +35,32 @@ class OtherPictureListItem extends StatelessWidget {
           decoration: BoxDecoration(
             boxShadow: [dropShadow()],
             borderRadius: BorderRadius.circular(15),
-            image: const DecorationImage(
-              image: NetworkImage(
-                "https://picsum.photos/300/300",
+          ),
+          child: CachedNetworkImage(
+            imageUrl: "https://picsum.photos/300/300",
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
               ),
-              fit: BoxFit.cover
+            ),
+            placeholder: (context, url) => Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.white,
+              child: Container(
+                width: 100.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            errorWidget: (context, url, error) => Image.asset(
+              "assets/images/img_square_placeholder.png",
+              fit: BoxFit.cover,
             )
           ),
         ),
