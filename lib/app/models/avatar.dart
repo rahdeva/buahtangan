311f8da53c72894def078fe6673f4183c223c581
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-21 12:18:23 +0800

    add null safety avatar model

--- lib/app/models/avatar.dart
+++ lib/app/models/avatar.dart
@@ -2,6 +2,8 @@
 //
 //     final avatar = avatarFromJson(jsonString);
 
+// ignore_for_file: prefer_if_null_operators
+
 import 'dart:convert';
 
 Avatar avatarFromJson(String str) => Avatar.fromJson(json.decode(str));
@@ -14,16 +16,16 @@ class Avatar {
         required this.avatarUrl,
     });
 
-    int id;
-    String avatarUrl;
+    int? id;
+    String? avatarUrl;
 
     factory Avatar.fromJson(Map<String, dynamic> json) => Avatar(
-        id: json["id"],
-        avatarUrl: json["avatarURL"],
+        id: json["id"] == null ? null : json["id"],
+        avatarUrl: json["avatarURL"] == null ? null : json["avatarURL"],
     );
 
     Map<String, dynamic> toJson() => {
-        "id": id,
-        "avatarURL": avatarUrl,
+        "id": id == null ? null : id,
+        "avatarURL": avatarUrl == null ? null : avatarUrl,
     };
 }
