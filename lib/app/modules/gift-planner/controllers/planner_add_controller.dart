41590a194b34faac71f929947983285769d7a8ea
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-24 19:51:17 +0800

    change login google button

--- lib/app/modules/gift-planner/controllers/planner_add_controller.dart
+++ lib/app/modules/gift-planner/controllers/planner_add_controller.dart
@@ -1,7 +1,85 @@
+import 'package:buahtangan/app/models/planner.dart';
+import 'package:buahtangan/app/widgets/snackbar/show_snackbar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PlannerAddController extends GetxController {
   static PlannerAddController find = Get.find();
+  TextEditingController nameC = TextEditingController();
+  TextEditingController messagesC = TextEditingController();
+  TextEditingController notesC = TextEditingController();
   TextEditingController testC = TextEditingController();
+
+  // void register() async {
+  //   if (nameC.text.isNotEmpty && phoneC.text.isNotEmpty && emailC.text.isNotEmpty && passC.text.isNotEmpty && passC2.text.isNotEmpty) {
+  //     if(passC.text == passC2.text){
+  //       isLoading.value = true;
+  //       final userCredential = await RegisterProvider().emailPasswordRegister(
+  //         emailC.text, passC.text
+  //       );
+  //       if (userCredential != null) {
+  //         final registeredUser = await RegisterProvider().createNewUser(
+  //           userCredential,
+  //           nameC.text, 
+  //           phoneC.text, 
+  //           emailC.text, 
+  //           passC.text
+  //         );
+  //         if (registeredUser == true) {
+  //           Get.toNamed(Routes.LOGIN);
+  //           showSnackbar(
+  //             "Berhasil Registrasi!", "Mohon cek email Anda untuk melakukan verifikasi.",
+  //             const Icon(Icons.check_circle_outline_rounded, color: Colors.green)
+  //           );
+  //         } else {
+  //           Get.back();
+  //           print("fill this later");
+  //         }
+  //       }
+  //       isLoading.value = false;
+  //     }
+  //     else{
+  //       showSnackbar(
+  //         "Oops!", "Password yang dimasukkan harus sama", 
+  //         const Icon(Icons.close_rounded, color: Colors.red)
+  //       );
+  //     }
+  //   } else {
+  //     showSnackbar(
+  //       "Oops!", "Semua input harus diisi.", 
+  //       const Icon(Icons.close_rounded, color: Colors.red)
+  //     );
+  //   }
+  // }
+
+  void createNewPlanner() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String uid = auth.currentUser!.uid;
+    PlannerData planner = PlannerData(
+      id: 2, 
+      createdAt: DateTime.now(), 
+      pictureUrl: "https://picsum.photos/500/500", 
+      receiver: "Jack Kahuna Laguna", 
+      date: DateTime.now(), 
+      event: "Birthday", 
+      budget: "100.000-100.000.000", 
+      notifDate: DateTime.now(), 
+      messages: "Give him a surprise", 
+      notes: "He is good surfer", 
+      giftSlugs: []
+    );
+    // try{
+    //   await FirebaseFirestore.instance.collection("planners").doc(uid).update({
+    //     "plannerData": FieldValue.arrayUnion(planner.toJson())
+    //   });
+    // }
+    // catch(e){
+    //   showSnackbar(
+    //     "Terjadi Kesalahan!", e.toString(),
+    //     const Icon(Icons.close_rounded, color: Colors.red)
+    //   );
+    // }
+  }
 }
---
+++ lib/app/modules/gift-planner/widgets/add_planner_picture_receiver.dart
@@ -0,0 +1,95 @@
+import 'package:buahtangan/app/modules/gift-planner/controllers/planner_add_controller.dart';
+import 'package:buahtangan/app/themes/color_theme.dart';
+import 'package:buahtangan/app/themes/decoration.dart';
+import 'package:buahtangan/app/themes/text_theme.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:sizer/sizer.dart';
+
+class AddPlannerPictureReceiver extends StatelessWidget {
+  const AddPlannerPictureReceiver({
+    Key? key, 
+    required this.controller,
+  }) : super(key: key);
+  
+  final PlannerAddController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                boxShadow: [dropShadow()],
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: CachedNetworkImage(
+                imageUrl: "https://picsum.photos/300/300",
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) => Shimmer.fromColors(
+                  baseColor: Colors.grey.shade300,
+                  highlightColor: Colors.white,
+                  child: Container(
+                    width: 100.w,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                errorWidget: (context, url, error) => Image.asset(
+                  "assets/images/img_square_placeholder.png",
+                  fit: BoxFit.cover,
+                )
+              ),
+            ),
+            Positioned(
+                bottom: -4,
+                right: -4,
+                child: CircleAvatar(
+                  backgroundColor: secondaryColor,
+                  radius: 12,
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: Icon(
+                      size: 9,
+                      Icons.edit,
+                      color: onSecondaryColor,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+        const SizedBox(width: 24),
+        SizedBox(
+          width: 100.w - 100 - 48 - 24,
+          child: TextField(
+            controller: controller.nameC,
+            decoration: const InputDecoration(
+              hintText: "Name",
+            ),
+            style: projectTextTheme.caption!.copyWith(
+              color: slate500
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
