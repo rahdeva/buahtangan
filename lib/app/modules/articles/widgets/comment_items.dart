cdf799cda07f441f950752fd5ea773c107508b9c
Ngurah Deva <wirandanaputra@gmail.com>
2023-01-29 09:19:11 +0800

    integration comment

--- lib/app/modules/articles/widgets/comment_items.dart
+++ lib/app/modules/articles/widgets/comment_items.dart
@@ -1,68 +1,69 @@
-import 'package:buahtangan/app/routes/app_pages.dart';
+import 'package:buahtangan/app/models/article.dart';
+import 'package:buahtangan/app/modules/articles/controllers/articles_detail_controller.dart';
 import 'package:buahtangan/app/themes/color_theme.dart';
 import 'package:buahtangan/app/themes/decoration.dart';
 import 'package:buahtangan/app/themes/text_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:sizer/sizer.dart';
 
 class CommentListItem extends StatelessWidget {
   final int index;
-  // final Hospital mData;
-  final controller;
+  final Comment mData;
+  final ArticlesDetailController controller;
 
   const CommentListItem({
-    Key? key, 
-    required this.index, 
-    // required this.mData,
-    this.controller, 
+    Key? key,
+    required this.index,
+    required this.mData,
+    required this.controller,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () {},
-        borderRadius: BorderRadius.circular(25.0),
-        child: Container(
-          width: 100.w,
-          margin: EdgeInsets.fromLTRB(0, index == 0 ? 16 : 0, 0, 16),
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          decoration: BoxDecoration(
-            color: surfaceColor,
-            borderRadius: BorderRadius.circular(25),
-            boxShadow: [dropShadow()]
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "User",
-                style: projectTextTheme.bodyText2!.copyWith(
-                  fontWeight: FontWeight.w600,
-                  color: onSurfaceColor
-                ),
-              ),
-              const SizedBox(height: 4),
-              Text(
-                "16 Juni 2022",
-                style: projectTextTheme.bodyText2!.copyWith(
-                  fontSize: 10,
-                  fontWeight: FontWeight.w400,
-                  color: slate500
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "Lorem ipsum dolor sit amet, consectetur Fames adipiscing elit. Fames tempus, ",
-                style: projectTextTheme.bodyText2!.copyWith(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  color: onSurfaceColor
-                ),
-              ),
-            ],
-          )
+      onTap: () {},
+      borderRadius: BorderRadius.circular(25.0),
+      child: Container(
+        width: 100.w,
+        margin: EdgeInsets.fromLTRB(0, index == 0 ? 16 : 0, 0, 16),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        decoration: BoxDecoration(
+          color: surfaceColor,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [dropShadow()]
         ),
-      );
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              mData.userName ?? "-",
+              style: projectTextTheme.bodyText2!.copyWith(
+                fontWeight: FontWeight.w600, 
+                color: onSurfaceColor
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              DateFormat("dd MMMM yyyy").format(mData.date ?? DateTime.now()),
+              style: projectTextTheme.bodyText2!.copyWith(
+                fontSize: 10, 
+                fontWeight: FontWeight.w400, 
+                color: slate500
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              mData.comment ?? "-",
+              style: projectTextTheme.bodyText2!.copyWith(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: onSurfaceColor
+              ),
+            ),
+          ],
+        )
+      ),
+    );
   }
 }
